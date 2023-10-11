7d748bd1c6f3ac448afe52e2aea9007f90f162ef
Tyler Hudson <tylerhudson96@gmail.com>
2023-10-11 18:58:24 -0400

    Refactored test to account for obj properties and their corresponding array lengths

--- gameboard.test.js
+++ gameboard.test.js
@@ -1,5 +1,12 @@
-test("Gameboard creation of 10 X 10 grid (array), length === 100", () => {
-  let playerBoard = gameboard();
+const { gameboard } = require("./gameboard");
 
-  expect(playerBoard.length).toEqual(100);
+test("Gameboard creation of 10 X 10 grid (array), length === 100", () => {
+  let exampleBoard = gameboard();
+  let examplePlayerBoard = exampleBoard.playerBoard.length;
+  let exampleEnemyBoard = exampleBoard.playerBoard.length;
+
+  expect(exampleBoard).toHaveProperty("playerBoard");
+  expect(examplePlayerBoard).toEqual(100);
+  expect(exampleBoard).toHaveProperty("enemyBoard");
+  expect(exampleEnemyBoard).toEqual(100);
 });
