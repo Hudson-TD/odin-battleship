fd4b2b3267ebdbe3739e4457438ba8f72c8f19ce
Tyler Hudson <tylerhudson96@gmail.com>
2023-10-11 16:37:06 -0400

    Refactored ship obj creation test to be more DRY

--- ship.test.js
+++ ship.test.js
@@ -1,63 +1,14 @@
 const { ship } = require("./ship");
 
-test("Ship factory returns Carrier object with properties name (string), length (int), hits (int), sunk (bool)", () => {
-  expect(ship("Carrier")).toStrictEqual({
-    name: "Carrier",
-    length: 5,
-    hits: 0,
-    sunk: false,
-  });
-});
-
-test("Ship factory returns Battleship object with properties name (string), length (int), hits (int), sunk (bool)", () => {
-  expect(ship("Battleship")).toStrictEqual({
-    name: "Battleship",
-    length: 4,
-    hits: 0,
-    sunk: false,
-  });
-});
-
-test("Ship factory returns Destroyer object with properties name (string), length (int), hits (int), sunk (bool)", () => {
-  expect(ship("Destroyer")).toStrictEqual({
-    name: "Destroyer",
-    length: 3,
-    hits: 0,
-    sunk: false,
-  });
-});
-
-test("Ship factory returns Submarine object with properties name (string), length (int), hits (int), sunk (bool)", () => {
-  expect(ship("Submarine")).toStrictEqual({
-    name: "Submarine",
-    length: 3,
-    hits: 0,
-    sunk: false,
-  });
-});
-
-test("Ship factory returns Patrol Boat object with properties name (string), length (int), hits (int), sunk (bool)", () => {
-  expect(ship("Patrol Boat")).toStrictEqual({
-    name: "Patrol Boat",
-    length: 2,
-    hits: 0,
-    sunk: false,
-  });
-});
-
-test("Ship hit method increments hit count by 1", () => {
-  let PB = ship("Patrol Boat");
-  expect(PB).toStrictEqual({
-    name: "Patrol Boat",
-    length: 2,
-    hits: 0,
-    sunk: false,
-  });
-
-  expect(PB.hit()).toStrictEqual({
-    name: "Patrol Boat",
-    length: 2,
-    hits: 1,
-    sunk: false,
-  });
+test("Ship factory returns all ship objects with properties name (string), length (int), hits (int), sunk (bool)", () => {
+  let newCarrier = ship("Carrier");
+  let newBattleship = ship("Battleship");
+  let newDestroyer = ship("Destroyer");
+  let newSubmarine = ship("Submarine");
+  let newPatrol = ship("Patrol Boat");
+  expect(newCarrier).toMatchObject({ ...newCarrier });
+  expect(newBattleship).toMatchObject({ ...newBattleship });
+  expect(newDestroyer).toMatchObject({ ...newDestroyer });
+  expect(newSubmarine).toMatchObject({ ...newSubmarine });
+  expect(newPatrol).toMatchObject({ ...newPatrol });
 });
