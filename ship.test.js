5ff5f41b889effae2d95b2d5a1f67d20e0d93dc8
Tyler Hudson <tylerhudson96@gmail.com>
2023-10-11 15:32:16 -0400

    Implemented additional tests to validate the return of the apporpriate object based on passed in ship type

--- ship.test.js
+++ ship.test.js
@@ -1,7 +1,45 @@
+const { ship } = require("./ship");
+
 test("Ship factory returns Carrier object with properties name (string), length (int), hits (int), sunk (bool)", () => {
-  expect(ship("Carrier")).toBe({
+  expect(ship("Carrier")).toStrictEqual({
     name: "Carrier",
     length: 5,
+    hits: 0,
+    sunk: false,
+  });
+});
+
+test("Ship factory returns Battleship object with properties name (string), length (int), hits (int), sunk (bool)", () => {
+  expect(ship("Battleship")).toStrictEqual({
+    name: "Battleship",
+    length: 4,
+    hits: 0,
+    sunk: false,
+  });
+});
+
+test("Ship factory returns Destroyer object with properties name (string), length (int), hits (int), sunk (bool)", () => {
+  expect(ship("Destroyer")).toStrictEqual({
+    name: "Destroyer",
+    length: 3,
+    hits: 0,
+    sunk: false,
+  });
+});
+
+test("Ship factory returns Submarine object with properties name (string), length (int), hits (int), sunk (bool)", () => {
+  expect(ship("Submarine")).toStrictEqual({
+    name: "Submarine",
+    length: 3,
+    hits: 0,
+    sunk: false,
+  });
+});
+
+test("Ship factory returns Patrol Boat object with properties name (string), length (int), hits (int), sunk (bool)", () => {
+  expect(ship("Patrol Boat")).toStrictEqual({
+    name: "Patrol Boat",
+    length: 2,
     hits: 0,
     sunk: false,
   });
