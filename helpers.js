47ea1e2c29efc4d825ac8ba9068f53f2b1680d33
Tyler Hudson <tylerhudson96@gmail.com>
2023-10-13 14:46:52 -0400

    Implemented helper function to randomly pick between a horizontal or vertical direction

--- helpers.js
+++ helpers.js
@@ -2,4 +2,19 @@ const randomNumZeroToNine = () => {
   return Math.floor(Math.random() * 10);
 };
 
-module.exports = { randomNumZeroToNine };
+const randomDirection = (start) => {
+  let direction;
+  let coinFlip = randomNumZeroToNine();
+
+  if (coinFlip <= 4) {
+    direction = "horizontal";
+  } else {
+    direction = "vertical";
+  }
+
+  return direction;
+};
+
+const determineSafeSpace = () => {};
+
+module.exports = { randomNumZeroToNine, randomDirection };
