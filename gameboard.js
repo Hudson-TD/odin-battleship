7a8defa8664d5f761e914719408a6acff613727d
Tyler Hudson <tylerhudson96@gmail.com>
2023-10-13 09:52:38 -0400

    Refactored gameboard to be an array of objects so additional properties can be implemented

--- gameboard.js
+++ gameboard.js
@@ -1,12 +1,12 @@
 const gameboard = () => {
   let xAxis = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
   let yAxis = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+  let gridArr = [];
 
   const generateGrid = (x, y) => {
-    let gridArr = [];
     for (let i = 0; i < 10; i++) {
       for (let j = 0; j < 10; j++) {
-        gridArr.push(`${x[i]}${y[j]}`);
+        gridArr.push({ coordinate: `${x[i]}${y[j]}` });
       }
     }
     return gridArr;
@@ -18,3 +18,5 @@ const gameboard = () => {
 };
 
 module.exports = { gameboard };
+
+console.log(gameboard());
--- gameboard.test.js
+++ gameboard.test.js
@@ -2,8 +2,8 @@ const { gameboard } = require("./gameboard");
 
 test("Gameboard creation of 10 X 10 grid (array), length === 100", () => {
   let exampleBoard = gameboard();
-  let examplePlayerBoard = exampleBoard.grid.length;
+  let boardSize = exampleBoard.grid.length;
 
   expect(exampleBoard).toHaveProperty("grid");
-  expect(examplePlayerBoard).toEqual(100);
+  expect(boardSize).toEqual(100);
 });
