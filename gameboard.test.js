c635a23bfb70eacac7f191b6795f5d5073c4b8a2
Tyler Hudson <tylerhudson96@gmail.com>
2023-10-13 10:28:42 -0400

    Refactored grid size test + implemented new test to validate randomly selected obj structure

--- gameboard.test.js
+++ gameboard.test.js
@@ -3,7 +3,17 @@ const { gameboard } = require("./gameboard");
 test("Gameboard creation of 10 X 10 grid (array), length === 100", () => {
   let exampleBoard = gameboard();
   let boardSize = exampleBoard.grid.length;
-
-  expect(exampleBoard).toHaveProperty("grid");
   expect(boardSize).toEqual(100);
+});
+
+test("Test random grid obj to match expected format", () => {
+  let exampleBoard = gameboard();
+  let randomGridEl = Math.floor(Math.random() * (100 - 1 + 1) + 1);
+  let expectedResult = {
+    coordinate: expect.any(Number),
+    shipName: undefined,
+    attacked: false,
+  };
+
+  expect(exampleBoard.grid[randomGridEl]).toMatchObject(expectedResult);
 });
