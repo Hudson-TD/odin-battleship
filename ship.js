26b78fcee0225e8c5c608b9323efb14d7f010c42
Tyler Hudson <tylerhudson96@gmail.com>
2023-10-13 15:40:27 -0400

    Implemented logic to randomly place ships on grid while obeying space constraints

--- ship.js
+++ ship.js
@@ -1,20 +1,26 @@
+const {
+  randomNumZeroToNine,
+  randomDirection,
+  determineSafePath,
+} = require("./helpers.js");
+
 const ship = (type) => {
   const length = (type) => {
     switch (type) {
       case "Carrier":
-        return 5;
+        return Number(5);
         break;
       case "Battleship":
-        return 4;
+        return Number(4);
         break;
       case "Destroyer":
-        return 3;
+        return Number(3);
         break;
       case "Submarine":
-        return 3;
+        return Number(3);
         break;
       case "Patrol Boat":
-        return 2;
+        return Number(2);
         break;
       default:
         throw new Error("Invalid ship type provided");
@@ -22,11 +28,57 @@ const ship = (type) => {
     }
   };
 
+  const generateCoordinates = (path, length, start) => {
+    let list = [];
+    list.push(start);
+    console.log(path);
+    console.log(length);
+
+    if (path == "left") {
+      console.log("going left");
+      for (let i = 1; i < length; i++) {
+        list.push({ x: start.x - i, y: start.y });
+      }
+    } else if (path == "right") {
+      console.log("going right");
+      for (let i = 1; i < length; i++) {
+        list.push({ x: start.x + i, y: start.y });
+      }
+    } else if (path == "up") {
+      console.log("going up");
+      for (let i = 1; i < length; i++) {
+        list.push({ x: start.x, y: start.y - i });
+      }
+    } else if (path == "down") {
+      console.log("going down");
+      for (let i = 1; i < length; i++) {
+        list.push({ x: start.x, y: start.y + i });
+      }
+    }
+    return list;
+  };
+
+  const randomPlacement = (shipLength) => {
+    let startingPoint = {
+      x: randomNumZeroToNine(),
+      y: randomNumZeroToNine(),
+    };
+
+    let direction = randomDirection();
+
+    let path = determineSafePath(direction, startingPoint);
+
+    let shipCoordinates = generateCoordinates(path, shipLength, startingPoint);
+
+    return shipCoordinates;
+  };
+
   return {
     name: type,
     length: length(type),
     hits: 0,
     sunk: false,
+    location: randomPlacement(length(type)),
     hit() {
       this.hits += 1;
       this.isSunk();
