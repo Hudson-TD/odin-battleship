eb5a2575238a6b1fbe453329c52c6a9c43bb9071
Tyler Hudson <tylerhudson96@gmail.com>
2023-10-16 17:05:06 -0400

    Module introduced for controlling HTML generation for the application, contains only player creation menu currently

--- src/domController.js
+++ src/domController.js
@@ -1,15 +1,39 @@
-const domController = () => {
-  return {
-    init: function () {
-      const playerCreation = document.querySelector(
-        ".player-creation-container"
-      );
-      const playerBoard = document.querySelector(".player-gameboard-container");
-      const computerBoard = document.querySelector(
-        ".computer-gameboard-container"
-      );
-    },
-  };
-};
+function renderPlayerCreationEl() {
+  let body = document.querySelector("body");
+  let containerEl = document.createElement("div");
+  containerEl.classList.add("player-creation-container");
 
-module.exports = { domController };
+  let overviewText = document.createElement("p");
+
+  overviewText.innerText = `You are about to enter unfamiliar waters in an effort to neutralize enemy ships. Due to thick fog our strategy is simple, fire artillery and observe any contact. Intel suggests the enemy fleet contains 5 ships of varying sizes...
+  
+  Instructions:
+  
+  Using arial reconnaissance order fire on mapped coordinates. Hits will be recorded with an explosion marker, while misses will be indicated with a water marker.`;
+
+  let formEl = document.createElement("form");
+  formEl.setAttribute("name", "player-creation-form");
+  formEl.setAttribute("class", "player-creation-form");
+
+  let inputEl = document.createElement("input");
+  inputEl.setAttribute("id", "playerName");
+  inputEl.setAttribute("type", "text");
+  inputEl.setAttribute("min", "1");
+  inputEl.required = true;
+  inputEl.setAttribute("max", "25");
+  inputEl.setAttribute("placeholder", "Enter Player Name");
+
+  let btnEl = document.createElement("button");
+  btnEl.setAttribute("id", "playerSubmit");
+  btnEl.setAttribute("type", "submit");
+  btnEl.setAttribute("for", "player-creation-form");
+  btnEl.innerText = "Launch Fleet";
+
+  body.appendChild(containerEl);
+  containerEl.appendChild(formEl);
+  formEl.appendChild(overviewText);
+  formEl.appendChild(inputEl);
+  formEl.appendChild(btnEl);
+}
+
+module.exports = { renderPlayerCreationEl };
