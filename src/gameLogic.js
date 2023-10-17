cd426b1735d81d4b5e4de4c8f190e92d74f7b369
Tyler Hudson <tylerhudson96@gmail.com>
2023-10-17 15:28:53 -0400

    Added gameLogic file and moved logic previously in index.js to keep things clean

---
+++ src/gameLogic.js
@@ -0,0 +1,58 @@
+const {
+  renderPlayerCreationEl,
+  generateGameboardEl,
+} = require("./domController");
+const { player } = require("./player");
+
+const game = () => {
+  return {
+    playerOneName: undefined,
+    playerTwoName: "Computer",
+    playerOne: undefined,
+    PlayerTwo: undefined,
+    init: function () {
+      renderPlayerCreationEl();
+      this.cacheDom();
+      this.startEventListening();
+    },
+    cacheDom: function () {
+      this.formContainer = document.querySelector(".player-creation-container");
+      this.formSubmitBtn = document.getElementById("playerSubmit");
+      this.formInput = document.getElementById("playerName");
+    },
+    startEventListening: function () {
+      this.formSubmitBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        this.playerOneName = this.formInput.value;
+        this.formContainer.classList.add("hidden");
+        this.handleGameSetup();
+      });
+    },
+    handleGameSetup: function () {
+      this.playerOne = player(this.playerOneName);
+      this.PlayerTwo = player(this.playerTwoName);
+      generateGameboardEl(this.playerOne);
+      generateGameboardEl(this.PlayerTwo);
+      this.handlePlotting();
+      this.startAttackListening();
+    },
+    startAttackListening: function () {
+      const cells = document.querySelectorAll(".gameboard-tile");
+      cells.forEach((cell) => {
+        cell.addEventListener("click", (e) => {
+          e.preventDefault();
+          let x = e.target.getAttribute("data-x");
+          let y = e.target.getAttribute("data-y");
+          this.PlayerTwo.board.receiveAttack(x, y);
+          console.log(this.PlayerTwo.board.grid);
+        });
+      });
+    },
+    handlePlotting: function () {
+      this.playerOne.board.plotFleet();
+      this.PlayerTwo.board.plotFleet();
+    },
+  };
+};
+
+module.exports = { game };
