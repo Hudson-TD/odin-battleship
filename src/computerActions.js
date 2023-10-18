fd3201af6a214732436e6448121451b5d1c42d1e
Tyler Hudson <tylerhudson96@gmail.com>
2023-10-18 15:15:50 -0400

    Created module for AI opponent with logic to track past moves and only make legal attacks aganist the player

---
+++ src/computerActions.js
@@ -0,0 +1,32 @@
+const { removeMatchingObjects } = require("./helpers");
+
+function computerLegalAttack(player) {
+  let illegalMoves = [];
+  let attackOptions = [];
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      attackOptions.push({
+        x: i,
+        y: j,
+      });
+    }
+  }
+
+  player.board.missedShots.forEach((obj) => {
+    illegalMoves.push(obj);
+  });
+
+  player.board.hits.forEach((obj) => {
+    illegalMoves.push(obj);
+  });
+
+  let legalAttacks = removeMatchingObjects(attackOptions, illegalMoves);
+
+  let randomLegalAttack =
+    legalAttacks[Math.floor(Math.random() * legalAttacks.length)];
+
+  console.log(`Random attack: ${randomLegalAttack}`);
+  return randomLegalAttack;
+}
+
+module.exports = { computerLegalAttack };
